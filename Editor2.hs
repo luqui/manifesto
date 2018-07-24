fe5517a86f03383326d32dbc239b9cb0959eaa34
Luke Palmer <lrpalmer@gmail.com>
2018-07-24 00:56:09 -0700

    newtyped Handler

--- Editor2.hs
+++ Editor2.hs
@@ -11,10 +11,19 @@ data Input = ILeft | IRight | IUp | IDown | IDelete | IInsert Char
 -- a call, where you go in there to do some work (yielding a series of views),
 -- and then at some point you exit with a new version of whatever you made.
 
-type Handler a = Input -> Maybe (Action a)
+newtype Handler a = Handler { runHandler :: Input -> Maybe a }
+    deriving (Functor)
+
+instance Applicative Handler where
+    pure = Handler . pure . pure
+    Handler f <*> Handler g = Handler $ liftA2 (<*>) f g
+
+instance Alternative Handler where
+    empty = Handler $ pure empty
+    Handler f <|> Handler g = Handler $ liftA2 (<|>) f g
 
 data Action a
-    = Yield (View a) (Handler a)
+    = Yield (View a) (Handler (Action a))
     | Done a
     deriving (Functor)
 
@@ -22,31 +31,28 @@ data View a = View String a
     deriving (Functor)
 
 (>>>=) :: Action a -> (a -> Action a) -> Action a
-Yield v h >>>= f = Yield v (fmap (>>>= f) . h) 
+Yield v h >>>= f = Yield v (fmap (>>>= f) h) 
 Done x >>>= f = f x
 
 getValue :: View a -> a
 getValue (View _ x) = x
 
 mapView :: (View a -> View a) -> Action a -> Action a
-mapView f (Yield v h) = Yield (f v) (fmap (mapView f) . h)
+mapView f (Yield v h) = Yield (f v) (fmap (mapView f) h)
 mapView _ (Done x) = Done x
 
-withHandler :: Action a -> (a -> Handler a) -> Action a
-withHandler (Yield v h) h' = Yield v $ \i -> case h' (getValue v) i of
-    Nothing -> fmap (`withHandler` h') (h i)
+withHandler :: Action a -> (a -> Handler (Action a)) -> Action a
+withHandler (Yield v h) h' = Yield v . Handler $ \i -> case runHandler (h' (getValue v)) i of
+    Nothing -> fmap (`withHandler` h') (runHandler h i)
     Just a' -> Just a'
 withHandler (Done a) _ = Done a
 
 frame :: (a -> View a) -> (a -> Action a) -> (a -> Action a)
-frame viewer subf a = Yield (viewer a) $ \case
-    IDown -> Just $ subf a `withHandler` \a' -> \case
+frame viewer subf a = Yield (viewer a) . Handler $ \case
+    IDown -> Just $ subf a `withHandler` \a' -> Handler $ \case
         IUp -> Just $ frame viewer subf a'
         _ -> Nothing
     _ -> Nothing
-
-mergeHandlers :: Handler a -> Handler a -> Handler a
-mergeHandlers h h' i = h i <|> h' i
 
 infixr 5 <++>
 (<++>) :: View [a] -> View [a] -> View [a]
@@ -55,30 +61,29 @@ View s xs <++> View s' xs' = View (s ++ s') (xs ++ xs')
 emptyV :: String -> View [a]
 emptyV s = View s []
 
-emptyHandler :: Handler a
-emptyHandler = const Nothing
-
-cursor :: forall a. ([a] -> View [a]) -> Handler [a] -> [a] -> Action [a]
+cursor :: forall a. ([a] -> View [a]) -> Handler (Action [a]) -> [a] -> Action [a]
 cursor viewer inserth = go []
     where
     go pre post = Yield (viewer (reverse pre) <++> emptyV "|" <++> viewer post) $ 
-        mergeHandlers lrHandler insertHandler
+        lrHandler <|> insertHandler
         where
-        lrHandler ILeft | p:pre' <- pre = Just $ go pre' (p:post)
-        lrHandler IRight | p:post' <- post = Just $ go (p:pre) post'
-        lrHandler _ = Nothing
+        lrHandler = Handler $ \case
+            ILeft | p:pre' <- pre -> Just $ go pre' (p:post)
+            IRight | p:post' <- post -> Just $ go (p:pre) post'
+            _ -> Nothing
 
-        insertHandler :: Handler [a]
-        insertHandler i = fmap doh (inserth i)
+        insertHandler :: Handler (Action [a])
+        insertHandler = fmap doh inserth
             where
             doh x = mapView ((viewer (reverse pre) <++>) . (<++> emptyV "|" <++> viewer post)) x >>>= \xs -> go (reverse xs ++ pre) post
 
-char :: Handler Char
-char (IInsert ch) = Just (Done ch)
-char _ = Nothing
+char :: Handler (Action Char)
+char = Handler $ \case
+    IInsert ch -> Just (Done ch)
+    _ -> Nothing
 
-charS :: Handler String
-charS = (fmap.fmap.fmap) (:[]) char
+charS :: Handler (Action String)
+charS = (fmap.fmap) (:[]) char
 
 viewString :: String -> View String
 viewString x = View x x
@@ -102,5 +107,5 @@ main = do
                     'K' -> IUp
                     'D' -> IDelete
                     c   -> IInsert c
-        maybe (go a) go (h ch')
+        maybe (go a) go (runHandler h ch')
     go (Done _) = return ()
