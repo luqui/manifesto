8fdd775794eb0daf5b1f4b6121250ec779c545ef
Luke Palmer <lrpalmer@gmail.com>
2018-07-23 19:23:32 -0700

    Refining the model baby

--- Editor.hs
+++ Editor.hs
@@ -6,8 +6,8 @@
 
 import Prelude hiding (id, (.))
 import Control.Category
-import Control.Arrow (left)
 import System.IO (hSetBuffering, stdin, BufferMode(..))
+import Control.Applicative
 
 data Iso a b = Iso (a -> b) (b -> a)
 
@@ -25,18 +25,22 @@ inverse (Iso f f') = Iso f' f
 
 data Input = ILeft | IRight | IUp | IDown | IInsert Char
 
-data Editor a = forall s. Editor {
-    eCreate  :: a -> s,
-    eInput   :: Input -> s -> Either a s,
-    eView    :: s -> String
+-- A machine with a transition monad m, hidden state s,
+-- and observable state a.
+data Machine m s a = Machine {
+    mCreate  :: a -> s,
+    mObserve :: s -> a,
+    mInput   :: Input -> s -> m s 
   }
 
-mapE :: Iso a b -> Editor a -> Editor b
-mapE (Iso f f') (Editor {..}) = Editor {
-    eCreate = eCreate . f',
-    eInput  = \i s -> left f (eInput i s),
-    eView   = eView
+mapMachine :: Iso a b -> Machine m s a -> Machine m s b
+mapMachine (Iso f f') (Machine {..}) = Machine {
+    mCreate = mCreate . f',
+    mObserve = f . mObserve,
+    mInput = mInput
   }
+
+data Editor a = forall s. View s => Editor (Machine Maybe s a)
 
 class Default a where
     def :: a
@@ -60,35 +64,61 @@ instance (View a) => View (Maybe a) where
 instance (View a, View b) => View (a,b) where
     view (a,b) = view a ++ view b
 
-char :: Editor (Maybe Char)
-char = Editor {
-    eCreate = id,
-    eInput  = \i s -> case i of
-        IInsert c | Nothing <- s -> Right (Just c)
-        _ -> Left s,
-    eView   = view
+charMachine :: (Alternative m) => Machine Maybe (Maybe Char) (Maybe Char)
+charMachine = Machine {
+    mCreate = id,
+    mObserve = id,
+    mInput = \i s -> case i of
+        IInsert c | Nothing <- s -> pure (Just c)
+        IChange -> pure Nothing
+        _ -> empty
   }
 
-pair :: (View a, View b) => Editor a -> Editor b -> Editor (a,b)
-pair (Editor { eCreate = createA, eInput = inputA, eView = viewA }) 
-     (Editor { eCreate = createB, eInput = inputB, eView = viewB }) 
-    = Editor {
-        eCreate = \(a, b) -> Left (createA a, b),
-        eInput  = input,
-        eView = \case
-            Left (as, b) -> viewA as ++ view b
-            Right (a, bs) -> view a ++ viewB bs
-      }
-    where
-    input i (Left (as, b)) = case inputA i as of
-        Left a' -> 
-            let s' = Right (a', createB b) in
-            input i s'
-        Right as' -> Right (Left (as', b))
-    input i (Right (a, bs)) = case inputB i bs of
-        Left b' -> Left (a, b')
-        Right bs' -> Right (Right (a, bs'))
+char :: Editor (Maybe Char)
+char = Editor charMachine
 
+data PairState sa sb a b
+    = PairFocus a b
+    | LeftFocus sa b
+    | RightFocus a sb
+
+instance (View sa, View sb, View a, View b) => View (PairState sa sb a b) where
+    view (PairFocus a b) = "[" ++ view a ++ view b ++ "]"
+    view (LeftFocus sa b) = view sa ++ view b
+    view (RightFocus a sb) = view a ++ view sb
+
+pairMachine :: (Alternative m) 
+            => Machine m sa a -> Machine m sb b -> Machine m (PairState sa sb a b) (a,b)
+pairMachine ma mb = Machine {
+    mCreate = \(a,b) -> PairFocus a b,
+    mObserve = \case
+        PairFocus a b -> (a, b)
+        LeftFocus sa b -> (mObserve ma sa, b)
+        RightFocus a sb -> (a, mObserve mb sb),
+    mInput = input
+  }
+    where
+    input i (PairFocus a b) = case i of
+        IInsert{} -> input i (LeftFocus (mCreate ma a) b)
+        IDown -> pure $ LeftFocus (mCreate ma a) b
+        _ -> empty
+    input i (LeftFocus sa b) = (LeftFocus <$> mInput ma i sa <*> pure b) <|> (
+        let rightfocus = RightFocus (mObserve ma sa) (mCreate mb b) in
+        case i of 
+            IRight -> pure rightfocus
+            IUp    -> pure $ PairFocus (mObserve ma sa) b
+            IInsert ch -> input (IInsert ch) rightfocus
+            _ -> empty)
+    input i (RightFocus a sb) = (RightFocus <$> pure a <*> mInput mb i sb) <|> (
+        case i of 
+            ILeft -> pure $ LeftFocus (mCreate ma a) (mObserve mb sb)  
+            IUp   -> pure $ PairFocus a (mObserve mb sb)
+            _ -> empty)
+
+pair :: (View a, View b) => Editor a -> Editor b -> Editor (a,b)
+pair (Editor ma) (Editor mb) = Editor (pairMachine ma mb)
+
+test :: Editor (Maybe Char, Maybe Char)
 test = pair char char
 
 main :: IO ()
@@ -96,12 +126,12 @@ main = do
     hSetBuffering stdin NoBuffering
     go test
   where
-    go (Editor {..}) = do
-        let s = eCreate def
+    go (Editor m) = do
+        let s = mCreate m def
         step s
       where
         step s = do
-            putStrLn $ eView s
+            putStrLn $ view s
             i <- getChar >>= return . \case
                         'H' -> ILeft
                         'J' -> IDown
@@ -109,6 +139,6 @@ main = do
                         'L' -> IRight
                         c   -> IInsert c
             putStrLn ""
-            case eInput i s of
-                Left r -> putStrLn $ view r
-                Right s' -> step s'
+            case mInput m i s of
+                Nothing -> putStrLn . view $ mObserve m s
+                Just s' -> step s'
