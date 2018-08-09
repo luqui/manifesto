29c64869fa160d16b3b394341b5ea06d0d452424
Luke Palmer <lrpalmer@gmail.com>
2018-08-09 15:58:34 -0700

    Super recursive higher order presentation

--- Paths.hs
+++ Paths.hs
@@ -20,6 +20,8 @@ instance Applicative (Input i) where
     pure = return
     (<*>) = ap
 
+type Command = Char
+
 
 newtype SemiLens a b = SemiLens { runSemiLens :: a -> Maybe (b, b -> a) }
 
@@ -30,86 +32,42 @@ instance Category SemiLens where
         (z,ctob) <- g y
         return (z, btoa . ctob)
 
+class Node n where
+    data Index n :: (* -> *) -> * -> *
 
-class Node a where
-    data Link a :: * -> *
-
-    linkLens :: Link a b -> SemiLens a b
-    
-
-data Path a b where
-    PNil :: Path a a
-    PLink :: (Node b) => Path a b -> Link b c -> Path a c
-
-pathLens :: Path a b -> SemiLens a b
-pathLens PNil = id
-pathLens (PLink p l) = linkLens l . pathLens p
-
-data Located a b = Located a (Path a b)
-
-data Ex c f where
-    Ex :: (c a) => f a -> Ex c f
-
-newtype Editor a = Editor { runEditor :: forall b. (Node b) => Located a b -> Ex Node (Located a) }
+    indexLens :: Index n f a -> SemiLens (n f) (f a)
 
 
 newtype Name = Name String
 
-data Program 
-    = Program [Defn]
-
-instance Node Program where
-    data Link Program t where 
-        LProgram :: Int -> Link Program Defn
-
-    linkLens (LProgram n) = SemiLens $ \case
-        Program defns
-            | 0 < n && n < length defns -> Just (defns !! n, \defn' -> Program (take n defns ++ [defn'] ++ drop (n+1) defns))
-            | otherwise -> Nothing
-
-
-data Defn
-    = Defn Name Expr
-
-instance Node Defn where
-    data Link Defn t where
-        LDefnName :: Link Defn Name
-        LDefnExpr :: Link Defn Expr
-
-    linkLens LDefnName = SemiLens $ \case
-        Defn name expr -> Just (name, \name' -> Defn name' expr)
-    linkLens LDefnExpr = SemiLens $ \case
-        Defn name expr -> Just (expr, \expr' -> Defn name expr')
-
-
-data Expr
-    = AddExpr Expr Expr
-    | NegExpr Expr
-    | VarExpr Name
-    | LitExpr Integer
+data Expr f
+    = AddExpr (f (Expr f)) (f (Expr f))
+    | NegExpr (f (Expr f))
+    | VarExpr (f Name)
+    | LitExpr (f Integer)
 
 
 instance Node Expr where
-    data Link Expr t where
-        LExprAddLeft :: Link Expr Expr
-        LExprAddRight :: Link Expr Expr
-        LExprNeg :: Link Expr Expr
-        LExprVar :: Link Expr Name
-        LExprLit :: Link Expr Integer
+    data Index Expr f t where
+        LExprAddLeft :: Index Expr f (Expr f)
+        LExprAddRight :: Index Expr f (Expr f)
+        LExprNeg :: Index Expr f (Expr f)
+        LExprVar :: Index Expr f Name
+        LExprLit :: Index Expr f Integer
 
-    linkLens LExprAddLeft = SemiLens $ \case
+    indexLens LExprAddLeft = SemiLens $ \case
         AddExpr a b -> Just (a, \a' -> AddExpr a' b)
         _ -> Nothing
-    linkLens LExprAddRight = SemiLens $ \case
+    indexLens LExprAddRight = SemiLens $ \case
         AddExpr a b -> Just (b, \b' -> AddExpr a b')
         _ -> Nothing
-    linkLens LExprNeg = SemiLens $ \case
+    indexLens LExprNeg = SemiLens $ \case
         NegExpr a -> Just (a, \a' -> NegExpr a')
         _ -> Nothing
-    linkLens LExprVar = SemiLens $ \case
+    indexLens LExprVar = SemiLens $ \case
         VarExpr v -> Just (v, \v' -> VarExpr v')
         _ -> Nothing
-    linkLens LExprLit = SemiLens $ \case
+    indexLens LExprLit = SemiLens $ \case
         LitExpr z -> Just (z, \z' -> LitExpr z')
         _ -> Nothing
 
