42ef8f16d18c888322d7468ecea395a1ad176649
Luke Palmer <lrpalmer@gmail.com>
2018-08-11 00:41:49 -0700

    Pretty nice motion

--- Sexp.hs
+++ Sexp.hs
@@ -1,16 +1,17 @@
-{-# OPTIONS -fdefer-type-errors -Wall #-}
+{-# OPTIONS -Wall #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns #-}
 
 module Sexp where
 
 import qualified Data.Text.Prettyprint.Doc as PP
 import qualified Data.Text.Prettyprint.Doc.Render.Terminal as PP
 import qualified System.Console.ANSI as ANSI
-import Data.Monoid (Monoid(..), First(..), mconcat)
+import Data.Monoid (Monoid(..), First(..), (<>), mconcat)
 import System.IO (hSetBuffering, stdin, BufferMode(..))
 
 data Sexp h = Sexp h [Sexp h]
@@ -20,7 +21,7 @@ data Frame h = Frame h [Sexp h] [Sexp h]
     deriving (Show)
 
 fillFrame :: Frame h -> Sexp h -> Sexp h
-fillFrame (Frame h l r) e = Sexp h (l ++ [e] ++ r)
+fillFrame (Frame h l r) e = Sexp h (reverse l ++ [e] ++ r)
 
 type Context h = [Frame h]   -- inside-out
 
@@ -63,27 +64,42 @@ newtype Editor i h = Editor { runEditor :: i -> Located h -> First (Located h) }
 
 basicMotion :: Editor Char h
 basicMotion = mconcat  
-    [ entry 'j' down
-    , entry 'k' up
-    , entry 'h' left
-    , entry 'l' right
+    [ entry 'k' up
+    , entry 'h' synleft
+    , entry 'l' synright
     ]
     where
-    up (Located (f:fs) e) = Just $ Located fs (fillFrame f e)
-    up _ = Nothing
+    up (Located (f:fs) e) = First . Just $ Located fs (fillFrame f e)
+    up _ = mempty
 
-    down (Located fs (Sexp h (e:es))) = Just $ Located (Frame h [] es : fs) e
-    down _ = Nothing
+    downFirst (Located fs (Sexp h (e:es))) = First . Just $ Located (Frame h [] es : fs) e
+    downFirst _ = mempty
 
-    left (Located (Frame h (l:ls) rs : fs) e) = Just $ Located (Frame h ls (e:rs) : fs) l
-    left _ = Nothing
+    downLast (Located fs (Sexp h (reverse -> e:es))) = First . Just $ Located (Frame h es [] : fs) e
+    downLast _ = mempty
 
-    right (Located (Frame h ls (r:rs) : fs) e) = Just $ Located (Frame h (e:ls) rs : fs) r
-    right _ = Nothing
+    left (Located (Frame h (l:ls) rs : fs) e) = First . Just $ Located (Frame h ls (e:rs) : fs) l
+    left _ = mempty
+
+    right (Located (Frame h ls (r:rs) : fs) e) = First . Just $ Located (Frame h (e:ls) rs : fs) r
+    right _ = mempty
+
+    synright = downFirst <> right <> r
+        where
+        -- this will terminate because at some point up will fail
+        r = up `andThen` (right <> r)
+
+    synleft = (left `andThen` drillDown) <> up
+        where
+        drillDown = (downLast `andThen` drillDown) <> nomotion
+
+    andThen f g l = First $ getFirst (f l) >>= getFirst . g
+
+    nomotion = First . Just
 
     entry c f = Editor $ \i l -> 
-        if | i == c -> First (f l)
-           | otherwise -> First Nothing
+        if | i == c -> f l
+           | otherwise -> mempty
 
 example :: Located String
 example = Located [] $ Sexp "doc" [r, Sexp "subbby" [r], r]
