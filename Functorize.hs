bc4f2553891eb5d584652efff0a10d368189ff41
Luke Palmer <lrpalmer@gmail.com>
2018-08-31 15:12:11 -0700

    Some ideas about higher order standard typeclasses

--- Functorize.hs
+++ Functorize.hs
@@ -16,12 +16,13 @@ import Data.Functor.Identity
 --
 -- into
 --
---    data TreeF a f = Leaf (f a) | Node (f (Tree a)) (f (Tree a))
+--    data TreeF a f = LeafF (f a) | NodeF (f (Tree a)) (f (Tree a))
 --
 -- and derive any HFunctor/HFoldable/etc instances.
 -- 
 -- Notice that we do not descend into the recursive trees.  This is one of the
--- things that makes working with functorized types nice, actually.
+-- things that makes working with functorized types nice, actually; we can
+-- reason about structures one recursive level at a time.
 
 data Tree a = Leaf a | Branch (Tree a) (Tree a)
     deriving Generic
@@ -82,3 +83,27 @@ instance (ShapableG f, ShapableG g) => ShapableG (f :*: g) where
     type ShapedG (f :*: g) = ShapedG f :*: ShapedG g 
     toShapeG (x :*: y) = toShapeG x :*: toShapeG y
     fromShapeG (x :*: y) = fromShapeG x :*: fromShapeG y
+
+
+class (HFunctor h) => HApplicative h where
+    hpure :: f () -> h f
+    hpair :: h f -> h g -> h (f :*: g)
+
+newtype f ~> g = NT { getNT :: forall x. f x -> g x }
+newtype (f --> g) x = Exp { getExp :: f x -> g x }
+
+hcurry :: (f :*: g) ~> h -> f ~> (g --> h)
+hcurry (NT m) = NT (\x -> Exp (\y -> m (x :*: y)))
+
+happly :: (HApplicative h) => h (a --> b) -> h a -> h b
+happly f x = hfmap (\(x :*: y) -> getExp x y) (hpair f x)
+
+
+class HTraversable h where
+    hsequenceA :: (Applicative f) => h (f :.: g) -> f (h g)
+
+data HTree a f = HLeaf (f a) | HBranch (f (Tree a)) (f (Tree a))
+
+instance HTraversable (HTree a) where
+    hsequenceA (HLeaf (Comp1 a)) = HLeaf <$> a
+    hsequenceA (HBranch (Comp1 a) (Comp1 b)) = HBranch <$> a <*> b
