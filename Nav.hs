eb7fbf31530061946fb467a8b4d76357ee6a5807
Luke Palmer <lrpalmer@gmail.com>
2018-09-14 10:08:17 -0700

    Wrote the rather difficult function 'siblings'

--- Nav.hs
+++ Nav.hs
@@ -10,8 +10,10 @@
 
 module Nav where
 
+import           Data.Bifunctor (first)
 import           Data.Constraint ((:-)(..), Dict(..))
 import qualified Data.Functor.Differentiable.Rank2 as D
+import           Data.Monoid (First(..), Dual(..))
 import           Data.Proxy (Proxy(..))
 import           Grammar
 import qualified Rank2
@@ -26,31 +28,50 @@ instance (Semantics f h) => Semantics (Annotated f) h where
 
 newtype c := c' = Sub2 (forall x. c x :- c' x)
 
+type NavAssumptions f h = (Rank2.Foldable h, D.Differentiable h, Semantics f h)
+
 class Navable f where
-    isFoldable :: f (L h) -> Dict (Rank2.Foldable h)
-    isDifferentiable :: f (L h) -> Dict (D.Differentiable h)
-    isSemantics :: f (L h) -> Dict (Semantics f h)
+    navAssumptions :: f (L h) -> Dict (NavAssumptions f h)
 
 mapAnnotated :: (Navable f) => (forall x. f x -> f' x) -> Annotated f x -> Annotated f' x
 mapAnnotated f (Annotated c h)
-    | Dict <- isSemantics c -- to get the Rank2.Functor instance
+    | Dict <- navAssumptions c
     = Annotated (f c) (Rank2.fmap (mapAnnotated f) h)
 
+data DAnnotated f h l = DAnnotated (f (L h)) (D.D h l (Annotated f))
+                                 -- ^ Should be a D f, once we have rank 1 derivatives. 
+
+dAnnNavAssumptions :: (Navable f) => DAnnotated f h l -> Dict (NavAssumptions f h)
+dAnnNavAssumptions (DAnnotated f _) = navAssumptions f
+
+fillDAnn :: (Navable f) => DAnnotated f h l -> Annotated f l -> Annotated f (L h)
+fillDAnn (DAnnotated f d) ann 
+    | Dict <- navAssumptions f
+    = sem (D.fill d ann)
 
 data Context f h l where
     CTop :: Context f (Only l) l
-    CCons :: (Semantics f h') => Context f h (L h') -> D.D h' l (Annotated f) -> Context f h l
+    CCons :: Context f h (L h') -> DAnnotated f h' l -> Context f h l
 
 data Zipper f h where
     Zipper :: Context f h l -> Annotated f l -> Zipper f h
 
-up :: Zipper f h -> Maybe (Zipper f h)
+up :: (Navable f) => Zipper f h -> Maybe (Zipper f h)
 up (Zipper CTop _) = Nothing
-up (Zipper (CCons cx d) t@Annotated{}) = Just (Zipper cx (sem (D.fill d t)))
+up (Zipper (CCons cx d) t) = Just (Zipper cx (fillDAnn d t))
 
-down :: Navable f => Zipper f h -> [Zipper f h]
+down :: (Navable f) => Zipper f h -> [Zipper f h]
 down (Zipper cx (Annotated f h)) 
-    | Dict <- isFoldable f
-    , Dict <- isDifferentiable f
-    , Dict <- isSemantics f
-    = Rank2.foldMap (\(D.Loc d x) -> [Zipper (CCons cx d) x]) (D.withLocs h)
+    | Dict <- navAssumptions f
+    = Rank2.foldMap (\(D.Loc d x) -> [Zipper (CCons cx (DAnnotated f d)) x]) (D.withLocs h)
+
+siblings :: (Navable f) => Zipper f h -> ([Zipper f h], [Zipper f h])
+siblings (Zipper CTop _) = ([],[])
+siblings (Zipper (CCons cx d@(DAnnotated f d')) t) 
+    | Dict <- dAnnNavAssumptions d
+    = first getDual . D.foldMapD (Dual . pure . getConst) (pure . getConst) $ 
+        Rank2.fmap (\(D.Loc dd t')  -> 
+            let dann = DAnnotated f (D.fill (D.commuteDs dd) t) in
+            Const (Zipper (CCons cx dann) t')
+        )
+        (D.withLocs d')
